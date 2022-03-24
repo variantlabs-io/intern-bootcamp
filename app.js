6f043c75d07a3df29e907675584314405e4c8c09
dheerajsharma0401 <dheeraj.sharma18@vit.edu>
2022-03-24 10:00:59 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,120 @@
+function myFunction1(){
+    window.history.replaceState({}, "Home", "/");
+    document.getElementById("mid").innerHTML=`<p><b> Star Sports</b> @StarSportsIndia·2h<br>
+    These 2⃣ icons are ready to take over the! Kyunki aise legends ka aana, #YehAbNormalHai! <br>
+    <br>Catch <a href="#">@RaviShastriOfc</a> & <a href="#">@ImRaina</a>with <a href="#">@MayantiLanger_B</a> chat about commentary, players & more!😍<br>
+    <br>Coming soon!<br><br><img  class="poster" src="https://pbs.twimg.com/media/FOhrWlLWYAECj19?format=jpg&name=small"></p>`; 
+    document.getElementById("mid-home").innerHTML="Home";
+    
+}
+
+function myFunction2(){
+    window.history.replaceState({}, "Explore", "/explore");
+
+    document.getElementById("mid").innerHTML=`Ukraine conflict<h3>LIVE</h3>
+        <p>Over 22,500 Indian citizens were evacuated from Ukraine,</p><h3>Trending</h3>1.Cricket . Trending<br><b>Starc</b><br>5,334 Tweets<br> 
+        <br>2·Politics · Trending<br><b>#OdishaLowestUnemployment</b><br>5,034 Tweets<br><br>3·Politics · Trending<br><b>#KejriwalKaChallenge</b><br>
+        5,872 Tweets<br><br>4·Cricket · Trending<br><b>#AUSvsPAK</b><br>2,382 Tweets<br><br>5·Trending<br>
+        <b>#CoinSwitchKaDoglapan</b><br>2,743 Tweets<br><br>6·Trending<br><b>#UnbelievableButTrue</b><br>7,107 Tweets<br><br>7·Trending<br>
+        <b>vishal mishra</b><br>4,160 Tweets<br><br>8·Movies & TV · Trending<br><b>PRATIKFAM FOR LIFE</b><br>198K Tweets<br><br>9·Entertainment · Trending<br>
+        <b>KYA KAR DIYA OUT TOMORROW</b><br>112K Tweets<br><br>10·Trending<br><b>BSN</b>L<br>1,809 Tweets<br><br>11·Trending<br><b>ICICI Lombard</b><br>
+        2,904 Tweets<br><br>12·Trending<br><b>Siddharth Trivedi</b><br><br>`
+        document.getElementById("mid-home").innerHTML="Explore";
+       
+    }
+
+    function myFunction3(){
+        window.history.replaceState({}, "Notifications", "/notifications");
+    
+        document.getElementById("mid").innerHTML=`<h2>Nothing to see here — yet</h2>When someone mentions you, you’ll find it here.`
+        document.getElementById("mid-home").innerHTML="Notifications";
+           
+        }
+
+
+     function myFunction4(){
+         window.history.replaceState({}, "Messages", "/messages");
+        
+        document.getElementById("mid").innerHTML=`<h2>Send a message, get a message</h2>Direct Messages are private conversations between you and other people on Twitter. Share Tweets, media, and more!`
+        document.getElementById("mid-home").innerHTML=`Messages`;
+
+               
+        }
+
+        function myFunction5(){
+            window.history.replaceState({}, "Bookmarks", "/bookmarks");
+           
+           document.getElementById("mid").innerHTML=`<center><img src="https://abs.twimg.com/sticky/illustrations/empty-states/book-in-bird-cage-800x400.v1.png" width=50%>
+           <h2>Save Tweets for later</h2>
+           Don’t let the good ones fly away! Bookmark<br> Tweets to easily find them again in the future.</center>`
+           document.getElementById("mid-home").innerHTML=`Bookmarks
+           <h6>@AkkaduSharma</h6>`;
+   
+                  
+        }
+
+        function myFunction6(){
+            window.history.replaceState({}, "Lists", "/lists");
+           
+           document.getElementById("mid").innerHTML=`<h3>Pinned Lists</h3>
+           Nothing to see here yet — pin your favorite Lists to access them quickly.
+           <h3>Discover new Lists</h3>
+          
+           Show more
+           Your Lists
+           You haven't created or followed any Lists. When you do, they'll show up here.
+           
+                   
+               `
+           document.getElementById("mid-home").innerHTML=`Lists<h6>@AkkaduSharma</h6>`;
+   
+                  
+        }
+
+        function myFunction7(){
+            window.history.replaceState({}, "Profile", "/profile");
+           
+           document.getElementById("mid").innerHTML=`
+           
+                <h4>Dheeraj Sharma</h4>
+                @AkkaduSharma
+                .
+                Joined May 2020<br>
+                46 Following  10 Followers <br>
+
+                <p><b>Tweets &nbsp; &nbsp;&nbsp;&nbsp; Tweets & replies &nbsp;&nbsp;&nbsp;&nbsp; Media &nbsp;&nbsp;&nbsp; &nbsp;Likes</b></p>
+                <hr>
+                <h3>Dheeraj Sharma's Tweet</h3>.
+                            `
+           document.getElementById("mid-home").innerHTML=`<h3>Dheeraj Sharma</h3>`;
+   
+                  
+        }
+
+        function myFunction8(){
+            window.history.replaceState({}, "More", "/more");
+           
+           document.getElementById("mid").innerHTML=`
+                <h3> Topics</h3>
+                <h3> Moments</h3>
+                <h3>Newsletters</h3>
+                <h3>Twitter for Professionals</h3>
+                <h3>Twitter Ads</h3>
+                <h3>Analytics</h3>
+                <h3>Settings and privacy</h3>
+                <h3>Help Center</h3>
+                <h3>Display</h3>
+                <h3>Keyboard shortcuts</h3>
+                        
+                            `
+           document.getElementById("mid-home").innerHTML=`<h3>More</h3>`;
+   
+                  
+        }
+
+
+ 
+
+
+
+            
